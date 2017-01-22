1bfdb4cd23d3f251ca34047a55900a624997f586
clementcisterne <lamouche444@hotmail.fr>
2017-01-23 00:47:16 +0100

    leave user

--- learnNodeJS/chat/server.js
+++ learnNodeJS/chat/server.js
@@ -4,34 +4,35 @@ var app = require('express')(),
     ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
     fs = require('fs');
 
-app.get('/', function(req,rep){
-    res.sendfile(__dirname + 'index.html');
+app.get('/', function(req,res){
+    res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function (socket, pseudo) {
 
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-
     socket.on('nouveau_client', function(pseudo) {
-
+        console.log(pseudo + " connected");
         pseudo = ent.encode(pseudo);
-
         socket.pseudo = pseudo;
-
         socket.broadcast.emit('nouveau_client', pseudo);
-
     });
-
 
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
-
     socket.on('message', function (message) {
-
+        console.log(message);
         message = ent.encode(message);
-
         socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
-
     });
+
+    // gestion des gens qui quittent le chat
+    socket.on('leave', function(pseudo){
+        console.log(pseudo + ' disconnect');
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo;     
+        socket.broadcast.emit('leave', pseudo);
+    });
+
 
 });
 
